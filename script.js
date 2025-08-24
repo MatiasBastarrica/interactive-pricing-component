8ff93b5efd1de43300e8ed9e487b75ae9a871e1c
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-08-23 22:39:31 -0300

    Change the price and the pageview count for the monthly billing option

--- script.js
+++ script.js
@@ -7,4 +7,34 @@ function updateSliderBackground(el) {
 
 updateSliderBackground(slider);
 
-slider.addEventListener("input", () => updateSliderBackground(slider));
+slider.addEventListener("input", () => {
+  updateSliderBackground(slider);
+  switch (slider.value) {
+    case "0":
+      pageview.textContent = "10K";
+      price.textContent = "$8";
+      break;
+    case "25":
+      pageview.textContent = "50K";
+      price.textContent = "$12";
+      break;
+    case "50":
+      pageview.textContent = "100K";
+      price.textContent = "$16";
+      break;
+    case "75":
+      pageview.textContent = "500K";
+      price.textContent = "$24";
+      break;
+    case "100":
+      pageview.textContent = "1M";
+      price.textContent = "$36";
+      break;
+
+    default:
+      break;
+  }
+});
+
+const pageview = document.querySelector(".pageview-number");
+const price = document.querySelector(".price");
